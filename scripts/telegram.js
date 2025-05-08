fbe5c6ed37496e5b4230320f3900fd828167bfef
ivan <yaloviyivan@gmail.com>
2025-05-08 12:41:55 +0600

    username

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -49,10 +49,6 @@ const user = JSON.parse(userParam);
 const username = user.username; // Вот username пользователя
 
 
-let usercard = document.querySelector(".header-top-right");
-
-let profUsername = document.createElement('p');
-profUsername.innerText = `${username}`;
-usercard.appendChild(profUsername);
-
+let usercard = document.querySelector(".header-top-left");
+document.body.insertAdjacentHTML("afterend", `<p class="telegram-username">${username}</p>`);
 
