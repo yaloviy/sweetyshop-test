73e87f3200e13e10e33e203118cccc7256208f58
ivan <yaloviyivan@gmail.com>
2025-05-08 14:34:53 +0600

    themefinaly

--- scripts/telegram.js
+++ scripts/telegram.js
@@ -39,3 +39,15 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
+
+
+
+const params = new URLSearchParams(tg.initData);
+const userParam = params.get('user');
+const user = JSON.parse(userParam);
+const username = user.username; // Вот username пользователя
+
+
+let usercard = document.querySelector(".header-container");
+usercard.insertAdjacentHTML("afterbegin", `<p class="telegram-username">${username}</p>`);
+
